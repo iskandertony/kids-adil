900157b6840751fe67f858bcebd703e98af6235d
Iska Mabo <maboiskanderr@gmail.com>
2025-06-08 18:31:52 +0600

    feat: git hub pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    basePath: '/kids-adil',
+    assetPrefix: '/kids-adil/',
 };
 
 export default nextConfig;
