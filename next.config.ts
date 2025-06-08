9eb9f09648ea317505d6323c62252a7f7aa6e5a8
Iska Mabo <maboiskanderr@gmail.com>
2025-06-08 20:01:44 +0600

    fix: vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-// next.config.js
-const basePath = '/kids-adil';
-const assetPrefix = '/kids-adil';
-
 export default {
     output: 'export',
-    basePath,
-    assetPrefix,
     images: {
         unoptimized: true,
     },
